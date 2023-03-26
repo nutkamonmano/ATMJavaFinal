4ff56ed445d41454560d53e1177591d2aa6652b7
Nutkamon Manosatcharak <651435@live.hcu.ac.th>
2023-03-27 01:34:32 +0700

    add logon scr

--- MainATM.java
+++ MainATM.java
@@ -12,6 +12,52 @@ class MainATM {
         OS.createStartupWindow();
     }
 
+    public void login(){
+        //ใช้ JPanel ในการสร้างโครงร่างในการทำ field รับค่าซึ่งใช้ JTextField
+        JPanel loginpanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        JTextField idlogin = new JTextField(10);
+        JTextField passlogin = new JTextField(10);
+
+        //สร้าง JFrame เพื่อนำทุกอย่างไปไว้ใน Frame
+        JFrame LOGIN = new JFrame("Login account");
+        LOGIN.setLayout(new GridLayout(3,1));
+        JPanel panel = new JPanel();
+        loginpanel.setLayout(new GridLayout(5,1));
+        JLabel idLg = new JLabel("Account ID: ");
+        JLabel passLg = new JLabel("Password: ");
+
+
+        panel.add(idLg);
+        panel.add(loginpanel.add(idlogin));
+        panel.add(passLg);
+        panel.add(loginpanel.add(passlogin));
+
+        JButton loginbtn = new JButton();
+        loginbtn.setText("LOG IN");
+        panel.add(loginbtn);
+        loginbtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e1){
+                for(int i=0; i<=countA; i++){
+                    if(idlogin.getText() == A[i].getAccID()){
+                        JOptionPane.showMessageDialog(null, "Correct");
+                        if(passlogin.getText() == A[i].getPassword()){
+                            JOptionPane.showMessageDialog(null, "Correct");
+                        }
+                    }
+                }
+            
+                LOGIN.dispose();
+            }
+        });
+
+        LOGIN.add(panel);
+
+        LOGIN.setSize(300, 300);
+        LOGIN.setLocationRelativeTo(null);
+        LOGIN.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        LOGIN.setVisible(true);
+    }
+
     public void createStartupWindow(){
 		JFrame frame = new JFrame();
         int result = JOptionPane.showConfirmDialog(frame, "Do you have an account already?", "User Confirmation",
@@ -57,6 +103,7 @@ class MainATM {
                 String PASS = pass.getText();
                 A[countA] = new Accounts(ID, NAME, PASS);
                 regisFrame.dispose();
+                OS.login();
             }
         });
 
