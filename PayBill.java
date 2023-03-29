2587e02563b281c26b011f7f9293206d8b852a32
Nutkamon Manosatcharak <651435@live.hcu.ac.th>
2023-03-30 02:18:22 +0700

    add menu on PayBill

--- PayBill.java
+++ PayBill.java
@@ -4,7 +4,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 public class PayBill extends MainATM {
     static int ch;
-    static PayBill pb = new PayBill();
 
     public static void main(String[] args){
         pb.menu();
@@ -14,51 +13,45 @@ public class PayBill extends MainATM {
         JFrame menu = new JFrame("Pay Bills");
         JPanel mpanel = new JPanel();
         JPanel mpanel2 = new JPanel();
-        String mm = "What bill(s) do you want to pay ?";
+        String mm = "คุณต้องการที่จะจ่ายบิลใด";
         JLabel mainmenu = new JLabel("<html><div style = 'text-align: center'><h1>"+mm+"</h1></div></html>",SwingConstants.LEFT);
-        JButton depbtn = new JButton("ค่าปรับ");
-        JButton wtdbtn = new JButton("Withdrawal");
-        JButton trfbtn = new JButton("Transfer");
-        JButton tupbtn = new JButton("Top-Up & Pay");
-        JButton logoutbtn = new JButton("Logout / Exit");
+        JButton finebtn = new JButton("ค่าปรับ");
+        JButton utilsbtn = new JButton("ค่าน้ำ/ค่าไฟ");
+        JButton phoneandinternetbtn = new JButton("ค่่าโทรศัพท์/ค่าอินเทอร์เน็ต");
+        JButton returnbtn = new JButton("กลับหน้าหลัก");
 
-        depbtn.addActionListener(new ActionListener(){
+        finebtn.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
                 JOptionPane.showMessageDialog(null, "Proceed to Deposit Section");
             }
         });
 
-        wtdbtn.addActionListener(new ActionListener(){
+        utilsbtn.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
                 JOptionPane.showMessageDialog(null, "Proceed to Withdrawal Section");
             }
         });
 
-        trfbtn.addActionListener(new ActionListener(){
+        phoneandinternetbtn.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
                 JOptionPane.showMessageDialog(null, "Proceed to Transfer Section");
             }
         });
 
-        tupbtn.addActionListener(new ActionListener(){
+        returnbtn.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
-                JOptionPane.showMessageDialog(null, "Proceed to Top-Up Section");
-            }
-        });
-
-        logoutbtn.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-                JOptionPane.showMessageDialog(null, "Proceed to Log Out Section");
+                menu.dispose();
+                OS.menu();
+                return;
             }
         });
 
         mpanel.setLayout(new GridLayout(4,2));
-        mpanel2.setLayout(new GridLayout(3,1));
-        mpanel.add(depbtn);
-        mpanel.add(wtdbtn);
-        mpanel.add(trfbtn);
-        mpanel.add(tupbtn);
-        mpanel2.add(logoutbtn, SwingConstants.CENTER);
+        mpanel2.setLayout(new GridLayout(1,1));
+        mpanel.add(finebtn);
+        mpanel.add(utilsbtn);
+        mpanel.add(phoneandinternetbtn);
+        mpanel2.add(returnbtn, SwingConstants.CENTER);
         
         menu.add(mainmenu, BorderLayout.NORTH);
         menu.add(mpanel, BorderLayout.CENTER);
